60b766fb64e289126f51fab41ece43dd89929057
Shamim Rahimi <shamimrahimi1383@gmail.com>
2024-06-27 00:24:26 +0330

    Update TB.v

--- verilog codes/TB.v
+++ verilog codes/TB.v
@@ -13,36 +13,36 @@ module TB;
     initial
         clk = 0;
     always
-        #20 clk = ~clk;
+        #25 clk = ~clk;
 
     initial begin
         reset <= 1;
         #45
         reset <= 0;
-        instruction <= {2'b10, 11'b0};
+        instruction <= 13'b1000000000000;  //  A4:A3 = A1 + A2
         #40
-        instruction <= {2'b11, 11'b0};
+        instruction <= 13'b1100000000000;  // A4:A3 = A1 * A2
         #40
-        instruction <= {4'b0000, 9'b0};
+        instruction <= 13'b0000000000001;  //  A1 <= memory[000000001]
         #40
-        instruction <= {4'b0001, 9'b10000};
+        instruction <= 13'b0001000001000;  //  A2 <= memory[000001000]
         #40
-        instruction <= {2'b10, 11'b0};
+        instruction <= 13'b1000000000000;  //  A4:A3 = A1 + A2
         #40
-        instruction <= {2'b11, 11'b0};
+        instruction <= 13'b0110000000001;  //  memory[000000001] <= A3
         #40
-        instruction <= {4'b0110, 9'b111111};
-        #40 
-        instruction <= {4'b0111, 9'b1111111};
+        instruction <= 13'b0111000001000;  //  memory[000001000] <= A4
         #40
-        instruction <= {4'b0000, 9'b111111};
+        instruction <= 13'b0000000000010;  //  A1 <= memory[000000010]
         #40
-        instruction <= {4'b0001, 9'b1111111};
+        instruction <= 13'b0001000000011;  //  A2 <= memory[000000011]
         #40
-        instruction <= {2'b10, 11'b0};
+        instruction <= 13'b1100000000000;  //  A4:A3 = A1 * A2
         #40
-        instruction <= {2'b11, 11'b0};
+        instruction <= 13'b0110000000010;  //  memory[000000010] <= A3
         #40
+        instruction <= 13'b0111000000011;  //  memory[000000011] <= A4
+
         $stop;
     end
 
@@ -51,3 +51,5 @@ module TB;
                     A1, A2, A3, A4);
 
 endmodule
+
+
