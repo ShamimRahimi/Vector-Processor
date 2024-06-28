d0334ef515236b817b9232a251475c4c393bd5df
Shamim Rahimi <shamimrahimi1383@gmail.com>
2024-06-28 13:07:49 +0330

    Update Memory.v

--- verilog codes/Memory.v
+++ verilog codes/Memory.v
@@ -1,39 +1,44 @@
 module Memory(
-    input wire clk,
-    input wire reset,
-    input wire write_en,
-    input wire read_en,
-    input [8:0] address, // 9 bits for 512 locations
-    input signed [511:0] write_data,
-    output reg signed [511:0] read_data
+    input wire clock,
+    input wire rst,
+    input wire wr_enable,
+    input wire rd_enable,
+    input [8:0] addr, // 9-bit address for 512 memory locations
+    input signed [511:0] wr_data,
+    output reg signed [511:0] rd_data
 );
 
-    reg [31:0] mem [0:511];
+    reg [31:0] memory_array [0:511];
+    integer k;
+    reg [8:0] write_addr;
+    reg [31:0] data_chunk;
 
-    always @(posedge clk) begin
-        if(reset) begin
-            mem[0] <= 32'h0;
-            mem[1] <= 32'h0;
-            mem[2] <= 32'hFFFFFFFF;
-            mem[3] <= 32'hF0000000;
-            mem[4] <= 32'hF0000000;
-            mem[5] <= 32'h0000000F;
-            mem[6] <= 32'h1;
-            mem[7] <= 32'h1;
-            mem[8] <= 32'h80000000;
-            mem[9] <= 32'h0F0000F0;
-            mem[10] <= 32'h0F0000F0;
-        end else begin
-            if (write_en) begin
-                for (i = 0; i < 16; i = i + 1) begin
-                    mem[(i + address) % 512] <= (write_data[32 * i +: 32]);
-                end
+    // Asynchronous reset and memory initialization
+    always @(posedge clock or posedge rst) begin
+        if (rst) begin
+            $readmemh("hex.txt", memory_array);
+        end
+    end
+
+    // Write operation
+    always @(posedge clock) begin
+        if (!rst && wr_enable) begin
+            for (k = 0; k < 16; k = k + 1) begin
+                write_addr = (addr + k) % 512;
+                data_chunk = wr_data[(k * 32) +: 32];
+                memory_array[write_addr] <= data_chunk;
             end
+        end
+    end
 
-            if (read_en) begin
-                for (j = 0; j < 16; j = j + 1) begin
-                    read_data[32 * j +: 32] = mem[(j + address) % 512];
-                end 
+    // Read operation
+    always @(posedge clock) begin
+        if (!rst && rd_enable) begin
+            rd_data = 'b0; // Clear read_data before assignment
+            for (k = 0; k < 16; k = k + 1) begin
+                read_addr = (addr + k) % 512;
+                temp_data = memory_array[read_addr];
+                rd_data[(k * 32) +: 32] <= temp_data;
             end
         end
     end
