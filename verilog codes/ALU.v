573bcdc7a0d8ddb4aa6bc7584d7bc3eb3b7dc9e0
Shamim Rahimi <shamimrahimi1383@gmail.com>
2024-06-28 13:07:13 +0330

    Update ALU.v

--- verilog codes/ALU.v
+++ verilog codes/ALU.v
@@ -2,28 +2,37 @@ module ALU(
     input signed [511:0] A1,
     input signed [511:0] A2,
     input wire [1:0] operation, // 00: Addition, 01: Multiplication
-    output signed [1023:0] output_data
+    output reg signed [1023:0] output_data
 );
 
-    reg [1023 : 0] ALUOut;
-    integer i, j;
-    assign output_data = ALUOut;
+    integer k;
+    reg [8:0] read_addr;
+    reg [31:0] data_chunk;
 
-    always @(*) begin
+    always @* begin
+        // Default assignment
+        output_data = 1024'b0;
+
+        // Perform operation based on operation select
         case (operation)
-            2'b00: begin
-                for (i = 0; i < 16; i = i + 1) begin
-                    ALUOut[i * 64 +: 64] <= (A1[i * 32 +: 32]) + (A2[i * 32 +: 32]);
+            2'b00: begin // Addition
+                for (k = 0; k<16; k=k+1) begin
+                    read_addr = i * 32 +: 32;
+                    data_chunk = A1[read_addr] + A2[read_addr];
+                    output_data[i * 64 +: 64] = data_chunk;
                 end
             end
-            2'b01: begin
-                for (j = 0; j < 16; j = j + 1) begin
-                    ALUOut[j * 64 +: 64] <= (A1[j * 32 +: 32]) * (A2[j * 32 +: 32]);
+            2'b01: begin // Multiplication
+                for (k = 0; k<16; k=k+1) begin
+                    read_addr = i * 32 +: 32;
+                    data_chunk = A1[read_addr] * A2[read_addr];
+                    output_data[i * 64 +: 64] = data_chunk;
                 end
             end
-            default: begin
-                ALUOut = 1024'b0;
+            default: begin // Default case (no operation)
+                output_data = 1024'b0;
             end
         endcase
     end
+
 endmodule
